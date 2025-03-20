1d0310384e746e18738b40563488897d46c1e0eb
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-03-21 02:50:23 +0300

    updated

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const price = document.getElementById("price");
-const tip = document.getElementById("tip-input");
+const tipInput = document.getElementById("tip-input");
 const output = document.getElementById("output");
 const btn = document.getElementById("calc");
 
 
-
+function percentageCalc(){
+    let tip = (tipInput.value / 100) * price.value;
+    let total = Number(price.value) + Number(tip);
+    output.innerHTML = total;
+    console.log(total);
+}
+btn.addEventListener("click", () => percentageCalc());
