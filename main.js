cb585b547e91bda1c549be1327b006443adbdc0d
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-03-21 02:29:52 +0300

    updated

---
+++ main.js
@@ -0,0 +1,5 @@
+const price = document.getElementById("price");
+const tip = document.getElementById("tip-input");
+const output = document.getElementById("output");
+const btn = document.getElementById("calc")
+
